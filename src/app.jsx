600d2bd7745509b9cd489cb682f1ad04769489df
sehee yun <seheeyun29@gmail.com>
2021-01-31 00:25:09 +0900

    ✨ navbar에 총 habit횟수 표시 구현

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
 import Habits from './components/habits';
 
-function App() {
-  return <Habits />;
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: 'Reading', count: 0 },
+      { id: 2, name: 'Running', count: 0 },
+      { id: 3, name: 'Coding', count: 0 },
+    ],
+    sum: 0,
+  };
+
+  habitsCount = () => {
+    const sum = this.state.habits.reduce(
+      (accumulator, currentValue) => accumulator + currentValue.count,
+      0
+    );
+    this.setState({ sum: sum });
+  };
+
+  hanledAdd = () => {};
+
+  render() {
+    return (
+      <>
+        <div className="navbar">
+          <i className="navbar-logo fas fa-feather-alt"></i>
+          Habit Tracker
+          <span className="navbar-count">{this.state.sum}</span>
+        </div>
+        <ul>
+          <input className="add-input" type="text" placeholder="Habit" />
+          <button className="add-button" onClick={this.hanledAdd}>
+            Add
+          </button>
+        </ul>
+        <Habits
+          habitsProps={this.state.habits}
+          onHabitsCount={this.habitsCount}
+        />
+        <button className="habits-reset">Reset All</button>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -2,23 +2,15 @@ import React, { Component } from 'react';
 import Habit from './habit';
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: 'Reading', count: 0 },
-      { id: 2, name: 'Running', count: 0 },
-      { id: 3, name: 'Coding', count: 0 },
-    ],
-  };
-
   handleIncrement = habit => {
-    const habits = [...this.state.habits];
+    const habits = [...this.props.habitsProps];
     const index = habits.indexOf(habit);
     habits[index].count++;
     this.setState({ habits: habits }); // key : value 가 같으면 생략해서 쓸 수 있다
   };
 
   handleDecrement = habit => {
-    const habits = [...this.state.habits];
+    const habits = [...this.props.habitsProps];
     const index = habits.indexOf(habit);
     habits[index].count === 0 || habits[index].count--;
     this.setState({ habits: habits });
@@ -30,9 +22,10 @@ class Habits extends Component {
   };
 
   render() {
+    const habits = this.props.habitsProps;
     return (
-      <ul className="habits-list">
-        {this.state.habits.map(habit => (
+      <ul className="habits-list" onClick={this.props.onHabitsCount}>
+        {habits.map(habit => (
           <Habit
             key={habit.id}
             habitProps={habit}
